7f705b731319ba8452688a72f0d7d85eb02111b6
petro.zarytskyi <petro.zarytskyi@gmail.com>
2025-01-24 14:48:19 +0100

    wip

--- include/clad/Differentiator/ReverseModeVisitor.h
+++ include/clad/Differentiator/ReverseModeVisitor.h
@@ -371,6 +371,7 @@ namespace clad {
     virtual StmtDiff VisitCompoundStmt(const clang::CompoundStmt* CS);
     StmtDiff VisitConditionalOperator(const clang::ConditionalOperator* CO);
     StmtDiff VisitCXXBoolLiteralExpr(const clang::CXXBoolLiteralExpr* BL);
+    StmtDiff VisitCXXBindTemporaryExpr(const clang::CXXBindTemporaryExpr* BTE);
     StmtDiff VisitCharacterLiteral(const clang::CharacterLiteral* CL);
     StmtDiff VisitStringLiteral(const clang::StringLiteral* SL);
     StmtDiff VisitCXXDefaultArgExpr(const clang::CXXDefaultArgExpr* DE);
@@ -404,6 +405,7 @@ namespace clad {
     StmtDiff VisitBreakStmt(const clang::BreakStmt* BS);
     StmtDiff
     VisitCXXStdInitializerListExpr(const clang::CXXStdInitializerListExpr* ILE);
+    StmtDiff VisitCXXTemporaryObjectExpr(const clang::CXXTemporaryObjectExpr* TOE);
     StmtDiff VisitCXXThisExpr(const clang::CXXThisExpr* CTE);
     StmtDiff VisitCXXNewExpr(const clang::CXXNewExpr* CNE);
     StmtDiff VisitCXXDeleteExpr(const clang::CXXDeleteExpr* CDE);
